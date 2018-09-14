2098ee2c9b1a285c1fd2b0f4c9daf8173288a8fd
markar46 <43162704+markar46@users.noreply.github.com>
2018-09-14 14:58:21 +0530

    Add files via upload

---
+++ grt.py
@@ -0,0 +1,14 @@
+#program to check lesser of 2 numbers
+#input a from keyboard
+a=int(input("enter value of a:"))
+#input b from keyboard
+b=int(input("enter value of b"))
+#print value of a
+print("a:",a)
+#print value of b
+print("b:",b)
+
+if(a<b):
+	print(a,"is lesser")
+else:
+	print(b,"is lesser")
