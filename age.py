340750cf2bd753fb588a080ad6691c89a8e4a9f1
markar46 <43162704+markar46@users.noreply.github.com>
2018-09-14 16:28:33 +0530

    Add files via upload

--- age.py
+++ age.py
@@ -1,9 +1,15 @@
-#program to check eligibility to vote
+#program to check the value of numbers
 #input a from keyboaed
 a=int(input("enter value of a:"))
+#print b from keyboaed
+b=int(input("enter value of b:"))
 #print value of a
 print("a:",a)
-if(a>=18):
-	print(a,"is eligible for vote")
+#input value of b
+print("b:",b)
+if(a>b):
+	print(a,"is greater ")
+elif(b>a):
+	print(b,"is greater")
 else:
-	print(a,"is not eligible for vote")
+	print("both are equal")
