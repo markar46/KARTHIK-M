c2c0d88e8dcc5838203455337d6fa39d816e8306
markar46 <43162704+markar46@users.noreply.github.com>
2018-09-14 15:40:31 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,9 @@
+#program to check eligibility to vote
+#input a from keyboaed
+a=int(input("enter value of a:"))
+#print value of a
+print("a:",a)
+if(a>=18):
+	print(a,"is eligible for vote")
+else:
+	print(a,"is not eligible for vote")
