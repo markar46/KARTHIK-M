2395ff7e09acbb68ef6b26995b494cbd5e2a2a54
markar46 <43162704+markar46@users.noreply.github.com>
2018-10-12 15:56:53 +0530

    Add files via upload

--- tk2.py
+++ tk2.py
@@ -1,41 +1,103 @@
-a=['1','2','3','4','5','6','7','8','9']
 
+
+		
+	
+a=['1','2','3','4','5','6','7','8','9']
 def printboard():
 	print(a[0]+'|'+a[1]+'|'+a[2])
-	print("----")
+	print("-----")
 	print(a[3]+'|'+a[4]+'|'+a[5])
-	print("----")
+	print("-----")
 	print(a[6]+'|'+a[7]+'|'+a[8])
-
 playeroneturn = True
-
-for i in range(9):
-	printboard()
-	#player 1 plays
-	if playeroneturn:
-		printboard()
-		p=input("player 1,choose your place:")
-		if p in a:
-			a[int(p)-1]='X'
-			playeroneturn= not playeroneturn
-		else:
-			p=input("player 2,choose your place:")
-			if p in a:
-				a[int(p)-1] = 'O'
-				playeroneturn = not playeroneturn
-			else:
-				print("computer's turn:")
-				sleep(1)
-				random_place()
-				playeroneturn= not playeroneturn
-			for x in range(0,3):
-				y=x*3
-				if(a[y]==a[(y+1)] and a[y]==a[(y+2)])
-				winner=True
+while True:
 				printboard()
-			if(a[x]==a[(x+3)] and a[x]==a[(x+6)])
-				winner=True
-				printboard()
-				if((a[0]==a[4] and a[0==a[8]) or a[2==a[4] and a[2==a[6]) 
+				p=input("choose an available space:")
 
-
+				if(p in a):
+								if (a[int(p)-1]=='X' or a[int(p)-1]=='O'):
+													print("place taken,choose another place...")
+													continue
+								else:
+													if playeroneturn:
+																	print("player 1>>")
+																	a[int(p)-1] = 'X'
+																	playeroneturn = not playeroneturn
+													else:
+																	print("player 2 >>")
+																	a[int(p)-1] = '0'
+																	playeroneturn = not playeroneturn
+													for i in(0,3,6):
+																	if(a[i]==a[i+1] and a[i]==a[i+2]):
+																					print("game over");
+																					exit()
+													for i in range(3):
+																	if(a[i]==a[i+3] and a[i]==a[i+6]):
+																					print("game over")
+																					exit()
+				else:
+								print("you have entered an invalid position")
+								1|2|3
+-----
+4|5|6
+-----
+7|8|9
+choose an available space:2
+player 1>>
+1|X|3
+-----
+4|5|6
+-----
+7|8|9
+choose an available space:9
+player 2 >>
+1|X|3
+-----
+4|5|6
+-----
+7|8|0
+choose an available space:5
+player 1>>
+1|X|3
+-----
+4|X|6
+-----
+7|8|0
+choose an available space:4
+player 2 >>
+1|X|3
+-----
+0|X|6
+-----
+7|8|0
+choose an available space:6
+player 1>>
+1|X|3
+-----
+0|X|X
+-----
+7|8|0
+choose an available space:1
+player 2 >>
+0|X|3
+-----
+0|X|X
+-----
+7|8|0
+choose an available space:7
+player 1>>
+0|X|3
+-----
+0|X|X
+-----
+X|8|0
+choose an available space:3
+player 2 >>
+0|X|0
+-----
+0|X|X
+-----
+X|8|0
+choose an available space:8
+player 1>>
+game over
